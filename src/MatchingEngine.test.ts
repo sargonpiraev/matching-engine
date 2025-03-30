bc857f213ea65415739b39178e385e5032e2a148
sargonpiraev <sargonpiraev@gmail.com>
2025-03-30 18:25:01 +0000

    add partial filling

--- src/MatchingEngine.test.ts
+++ src/MatchingEngine.test.ts
@@ -1,21 +1,27 @@
 import assert from 'node:assert'
 import { MatchingEngine } from './MatchingEngine'
 import { randomUUID } from 'node:crypto'
-import { Order, OrderSide } from './types'
+import { Order, OrderSide, Trade } from './types'
 
 describe('MarchingEngine Tests', () => {
-  test('#createOrder', () => {
+  test(`
+    given
+      no orders yet
+    when
+      new order added
+    then
+      order should be added to orders
+  `, () => {
     //assign
     const matchingEngine = new MatchingEngine()
-    const order: Order = { id: randomUUID(), side: OrderSide.BID, price: 1 }
+    const order: Order = { id: randomUUID(), side: OrderSide.BID, price: 1, quantity: 1 }
     // act
-    matchingEngine.createOrder(order)
+    matchingEngine.addOrder(order)
     // assert
-    assert.strictEqual(matchingEngine.orders.length, 1)
+    assert.strictEqual(matchingEngine.bids.length, 1)
   })
 
-  describe('#match', () => {
-    test(`
+  test(`
       given
         there are no orders and
       when
@@ -24,17 +30,17 @@ describe('MarchingEngine Tests', () => {
         no trade should happen and
         order should be added to orders
     `, () => {
-      //assign
-      const matchingEngine = new MatchingEngine()
-      const order: Order = { id: randomUUID(), side: OrderSide.BID, price: 1 }
-      // act
-      const result = matchingEngine.createOrder(order)
-      // assert
-      assert.strictEqual(result, undefined)
-      assert.strictEqual(matchingEngine.orders.length, 1)
-    })
+    //assign
+    const matchingEngine = new MatchingEngine()
+    const order: Order = { id: randomUUID(), side: OrderSide.BID, price: 1, quantity: 1 }
+    // act
+    const result = matchingEngine.addOrder(order)
+    // assert
+    assert.deepEqual(result, [])
+    assert.strictEqual(matchingEngine.bids.length, 1)
+  })
 
-    test(`
+  test(`
       given
         there is one bid order
       when
@@ -43,19 +49,19 @@ describe('MarchingEngine Tests', () => {
         no trade should happen and
         order should be added to bids
     `, () => {
-      //assign
-      const matchingEngine = new MatchingEngine()
-      const order1: Order = { id: randomUUID(), side: OrderSide.BID, price: 1 }
-      const order2: Order = { id: randomUUID(), side: OrderSide.BID, price: 1 }
-      matchingEngine.createOrder(order1)
-      // act
-      const result = matchingEngine.createOrder(order2)
-      // assert
-      assert.strictEqual(result, undefined)
-      assert.strictEqual(matchingEngine.orders.length, 2)
-    })
+    //assign
+    const matchingEngine = new MatchingEngine()
+    const order1: Order = { id: randomUUID(), side: OrderSide.BID, price: 1, quantity: 1 }
+    const order2: Order = { id: randomUUID(), side: OrderSide.BID, price: 1, quantity: 1 }
+    matchingEngine.addOrder(order1)
+    // act
+    const result = matchingEngine.addOrder(order2)
+    // assert
+    assert.deepEqual(result, [])
+    assert.strictEqual(matchingEngine.bids.length, 2)
+  })
 
-    test(`
+  test(`
       given
         there is one ask order
       when
@@ -64,19 +70,19 @@ describe('MarchingEngine Tests', () => {
         no trade should happen and
         order should be added to asks
     `, () => {
-      //assign
-      const matchingEngine = new MatchingEngine()
-      const order1: Order = { id: randomUUID(), side: OrderSide.ASK, price: 1 }
-      const order2: Order = { id: randomUUID(), side: OrderSide.ASK, price: 1 }
-      matchingEngine.createOrder(order1)
-      // act
-      const result = matchingEngine.createOrder(order2)
-      // assert
-      assert.strictEqual(result, undefined)
-      assert.strictEqual(matchingEngine.orders.length, 2)
-    })
+    //assign
+    const matchingEngine = new MatchingEngine()
+    const order1: Order = { id: randomUUID(), side: OrderSide.ASK, price: 1, quantity: 1 }
+    const order2: Order = { id: randomUUID(), side: OrderSide.ASK, price: 1, quantity: 1 }
+    matchingEngine.addOrder(order1)
+    // act
+    const result = matchingEngine.addOrder(order2)
+    // assert
+    assert.deepEqual(result, [])
+    assert.strictEqual(matchingEngine.asks.length, 2)
+  })
 
-    test(`
+  test(`
       given
         there is one bid order
       when
@@ -86,19 +92,20 @@ describe('MarchingEngine Tests', () => {
         trade should not happen and
         new ask order should be added to orders
     `, () => {
-      //assign
-      const matchingEngine = new MatchingEngine()
-      const bidOrder: Order = { id: randomUUID(), side: OrderSide.BID, price: 1 }
-      const askOrder: Order = { id: randomUUID(), side: OrderSide.ASK, price: 2 }
-      matchingEngine.createOrder(bidOrder)
-      // act
-      const result = matchingEngine.createOrder(askOrder)
-      // assert
-      assert.strictEqual(result, undefined)
-      assert.strictEqual(matchingEngine.orders.length, 2)
-    })
+    //assign
+    const matchingEngine = new MatchingEngine()
+    const bidOrder: Order = { id: randomUUID(), side: OrderSide.BID, price: 1, quantity: 1 }
+    const askOrder: Order = { id: randomUUID(), side: OrderSide.ASK, price: 2, quantity: 1 }
+    matchingEngine.addOrder(bidOrder)
+    // act
+    const result = matchingEngine.addOrder(askOrder)
+    // assert
+    assert.deepEqual(result, [])
+    assert.strictEqual(matchingEngine.bids.length, 1)
+    assert.strictEqual(matchingEngine.asks.length, 1)
+  })
 
-    test(`
+  test(`
       given
         there is one bid order
       when
@@ -108,20 +115,21 @@ describe('MarchingEngine Tests', () => {
         trade should happen and
         no orders should be in orders in the end
     `, () => {
-      //assign
-      const matchingEngine = new MatchingEngine()
-      const bidOrder: Order = { id: randomUUID(), side: OrderSide.BID, price: 1 }
-      const askOrder: Order = { id: randomUUID(), side: OrderSide.ASK, price: 1 }
-      matchingEngine.createOrder(bidOrder)
-      // act
-      const result = matchingEngine.createOrder(askOrder)
-      // assert
-      const trade = { bidOrderId: bidOrder.id, askOrderId: askOrder.id, price: 1 }
-      assert.deepEqual(result, trade)
-      assert.strictEqual(matchingEngine.orders.length, 0)
-    })
+    //assign
+    const matchingEngine = new MatchingEngine()
+    const bidOrder: Order = { id: randomUUID(), side: OrderSide.BID, price: 1, quantity: 1 }
+    const askOrder: Order = { id: randomUUID(), side: OrderSide.ASK, price: 1, quantity: 1 }
+    matchingEngine.addOrder(bidOrder)
+    // act
+    const result = matchingEngine.addOrder(askOrder)
+    // assert
+    const trade: Trade = { bidOrderId: bidOrder.id, askOrderId: askOrder.id, price: 1, quantity: 1 }
+    assert.deepEqual(result, [trade])
+    assert.strictEqual(matchingEngine.bids.length, 0)
+    assert.strictEqual(matchingEngine.asks.length, 0)
+  })
 
-    test(`
+  test(`
       given
         there is one bid order
       when
@@ -132,20 +140,26 @@ describe('MarchingEngine Tests', () => {
         price should be bid order price
         no orders should be in orders in the end
     `, () => {
-      //assign
-      const matchingEngine = new MatchingEngine()
-      const bidOrder: Order = { id: randomUUID(), side: OrderSide.BID, price: 2 }
-      const askOrder: Order = { id: randomUUID(), side: OrderSide.ASK, price: 1 }
-      matchingEngine.createOrder(bidOrder)
-      // act
-      const result = matchingEngine.createOrder(askOrder)
-      // assert
-      const trade = { bidOrderId: bidOrder.id, askOrderId: askOrder.id, price: bidOrder.price }
-      assert.deepEqual(result, trade)
-      assert.strictEqual(matchingEngine.orders.length, 0)
-    })
+    //assign
+    const matchingEngine = new MatchingEngine()
+    const bidOrder: Order = { id: randomUUID(), side: OrderSide.BID, price: 2, quantity: 1 }
+    const askOrder: Order = { id: randomUUID(), side: OrderSide.ASK, price: 1, quantity: 1 }
+    matchingEngine.addOrder(bidOrder)
+    // act
+    const result = matchingEngine.addOrder(askOrder)
+    // assert
+    const trade: Trade = {
+      bidOrderId: bidOrder.id,
+      askOrderId: askOrder.id,
+      price: bidOrder.price,
+      quantity: 1,
+    }
+    assert.deepEqual(result, [trade])
+    assert.strictEqual(matchingEngine.bids.length, 0)
+    assert.strictEqual(matchingEngine.asks.length, 0)
+  })
 
-    test(`
+  test(`
       given
         there are two bid orders and
         one bid order price is higher
@@ -157,26 +171,33 @@ describe('MarchingEngine Tests', () => {
         price should be the highest bid order price and
         bid order with less price should be in orders in the end
     `, () => {
-      //assign
-      const matchingEngine = new MatchingEngine()
-      const bidOrderLowPrice: Order = { id: randomUUID(), side: OrderSide.BID, price: 1 }
-      const bidOrderHighPrice: Order = { id: randomUUID(), side: OrderSide.BID, price: 2 }
-      const askOrder: Order = { id: randomUUID(), side: OrderSide.ASK, price: 1 }
-      matchingEngine.createOrder(bidOrderLowPrice)
-      matchingEngine.createOrder(bidOrderHighPrice)
-      // act
-      const result = matchingEngine.createOrder(askOrder)
-      // assert
-      const trade = {
-        bidOrderId: bidOrderHighPrice.id,
-        askOrderId: askOrder.id,
-        price: bidOrderHighPrice.price,
-      }
-      assert.deepEqual(result, trade)
-      assert.strictEqual(matchingEngine.orders.length, 1)
-    })
+    //assign
+    const matchingEngine = new MatchingEngine()
+    const bidOrderLowPrice: Order = { id: randomUUID(), side: OrderSide.BID, price: 1, quantity: 1 }
+    const bidOrderHighPrice: Order = {
+      id: randomUUID(),
+      side: OrderSide.BID,
+      price: 2,
+      quantity: 1,
+    }
+    const askOrder: Order = { id: randomUUID(), side: OrderSide.ASK, price: 1, quantity: 1 }
+    matchingEngine.addOrder(bidOrderLowPrice)
+    matchingEngine.addOrder(bidOrderHighPrice)
+    // act
+    const result = matchingEngine.addOrder(askOrder)
+    // assert
+    const trade: Trade = {
+      bidOrderId: bidOrderHighPrice.id,
+      askOrderId: askOrder.id,
+      price: bidOrderHighPrice.price,
+      quantity: 1,
+    }
+    assert.deepEqual(result, [trade])
+    assert.strictEqual(matchingEngine.bids.length, 1)
+    assert.strictEqual(matchingEngine.asks.length, 0)
+  })
 
-    test(`
+  test(`
       given
         there are two ask orders and
         one ask order price is higher
@@ -188,23 +209,185 @@ describe('MarchingEngine Tests', () => {
         price should be the bid order price and
         ask order with highest price should be in orders in the end
     `, () => {
-      //assign
-      const matchingEngine = new MatchingEngine()
-      const askOrderLowestPrice: Order = { id: randomUUID(), side: OrderSide.ASK, price: 1 }
-      const askOrderBigestPrice: Order = { id: randomUUID(), side: OrderSide.ASK, price: 2 }
-      const bidOrder: Order = { id: randomUUID(), side: OrderSide.BID, price: 1 }
-      matchingEngine.createOrder(askOrderLowestPrice)
-      matchingEngine.createOrder(askOrderBigestPrice)
-      // act
-      const result = matchingEngine.createOrder(bidOrder)
-      // assert
-      const trade = {
-        bidOrderId: bidOrder.id,
-        askOrderId: askOrderLowestPrice.id,
-        price: bidOrder.price,
-      }
-      assert.deepEqual(result, trade)
-      assert.strictEqual(matchingEngine.orders.length, 1)
-    })
+    //assign
+    const matchingEngine = new MatchingEngine()
+    const askOrderLowestPrice: Order = {
+      id: randomUUID(),
+      side: OrderSide.ASK,
+      price: 1,
+      quantity: 1,
+    }
+    const askOrderBigestPrice: Order = {
+      id: randomUUID(),
+      side: OrderSide.ASK,
+      price: 2,
+      quantity: 1,
+    }
+    const bidOrder: Order = { id: randomUUID(), side: OrderSide.BID, price: 1, quantity: 1 }
+    matchingEngine.addOrder(askOrderLowestPrice)
+    matchingEngine.addOrder(askOrderBigestPrice)
+    // act
+    const result = matchingEngine.addOrder(bidOrder)
+    // assert
+    const trade: Trade = {
+      bidOrderId: bidOrder.id,
+      askOrderId: askOrderLowestPrice.id,
+      price: bidOrder.price,
+      quantity: 1,
+    }
+    assert.deepEqual(result, [trade])
+    assert.strictEqual(matchingEngine.asks.length, 1)
+    assert.strictEqual(matchingEngine.bids.length, 0)
+  })
+
+  test(`
+      given
+        there is one bid order with quantity 10
+      when
+        new ask order added with quantity 5 and
+        ask order price matches bid order price
+      then
+        trade should happen for quantity 5 and
+        remaining quantity of bid order should be 5
+    `, () => {
+    //assign
+    const matchingEngine = new MatchingEngine()
+    const bidOrder: Order = { id: randomUUID(), side: OrderSide.BID, price: 1, quantity: 10 }
+    const askOrder: Order = { id: randomUUID(), side: OrderSide.ASK, price: 1, quantity: 5 }
+    matchingEngine.addOrder(bidOrder)
+    // act
+    const result = matchingEngine.addOrder(askOrder)
+    // assert
+    const trade: Trade = { bidOrderId: bidOrder.id, askOrderId: askOrder.id, price: 1, quantity: 5 }
+    assert.deepEqual(result, [trade])
+    assert.strictEqual(matchingEngine.bids.length, 1)
+    assert.strictEqual(matchingEngine.asks.length, 0)
+    assert.strictEqual(matchingEngine.bids[0].quantity, 5)
+  })
+
+  test(`
+      given
+        there is one ask order with quantity 10
+      when
+        new bid order added with quantity 5 and
+        bid order price matches ask order price
+      then
+        trade should happen for quantity 5 and
+        remaining quantity of ask order should be 5
+    `, () => {
+    //assign
+    const matchingEngine = new MatchingEngine()
+    const askOrder: Order = { id: randomUUID(), side: OrderSide.ASK, price: 1, quantity: 10 }
+    const bidOrder: Order = { id: randomUUID(), side: OrderSide.BID, price: 1, quantity: 5 }
+    matchingEngine.addOrder(askOrder)
+    // act
+    const result = matchingEngine.addOrder(bidOrder)
+    // assert
+    const trade: Trade = { bidOrderId: bidOrder.id, askOrderId: askOrder.id, price: 1, quantity: 5 }
+    assert.deepEqual(result, [trade])
+    assert.strictEqual(matchingEngine.asks.length, 1)
+    assert.strictEqual(matchingEngine.bids.length, 0)
+    assert.strictEqual(matchingEngine.asks[0].quantity, 5)
+  })
+
+  test(`
+      given
+        there is one bid order with quantity 5
+      when
+        new ask order added with quantity 10 and
+        ask order price matches bid order price
+      then
+        trade should happen for quantity 5 and
+        remaining quantity of ask order should be 5
+    `, () => {
+    //assign
+    const matchingEngine = new MatchingEngine()
+    const bidOrder: Order = { id: randomUUID(), side: OrderSide.BID, price: 1, quantity: 5 }
+    const askOrder: Order = { id: randomUUID(), side: OrderSide.ASK, price: 1, quantity: 10 }
+    matchingEngine.addOrder(bidOrder)
+    // act
+    const result = matchingEngine.addOrder(askOrder)
+    // assert
+    const trade: Trade = { bidOrderId: bidOrder.id, askOrderId: askOrder.id, price: 1, quantity: 5 }
+    assert.deepEqual(result, [trade])
+    assert.strictEqual(matchingEngine.bids.length, 0)
+    assert.strictEqual(matchingEngine.asks.length, 1)
+    assert.strictEqual(matchingEngine.asks[0].quantity, 5)
+  })
+
+  test(`
+      given
+        there are two bid orders with quantities 5 and 10 respectively
+      when
+        new ask order added with quantity 12 and
+        ask order price matches bid order prices
+      then
+        trade should happen for quantity 12 and
+        first bid order should fully execute and
+        second bid order should have remaining quantity 3
+    `, () => {
+    //assign
+    const matchingEngine = new MatchingEngine()
+    const bidOrder1: Order = { id: randomUUID(), side: OrderSide.BID, price: 1, quantity: 5 }
+    const bidOrder2: Order = { id: randomUUID(), side: OrderSide.BID, price: 1, quantity: 10 }
+    const askOrder: Order = { id: randomUUID(), side: OrderSide.ASK, price: 1, quantity: 12 }
+    matchingEngine.addOrder(bidOrder1)
+    matchingEngine.addOrder(bidOrder2)
+    // act
+    const result = matchingEngine.addOrder(askOrder)
+    // assert
+    const trade1: Trade = {
+      bidOrderId: bidOrder1.id,
+      askOrderId: askOrder.id,
+      price: 1,
+      quantity: 5,
+    }
+    const trade2: Trade = {
+      bidOrderId: bidOrder2.id,
+      askOrderId: askOrder.id,
+      price: 1,
+      quantity: 7,
+    }
+    assert.deepEqual(result, [trade1, trade2])
+    assert.strictEqual(matchingEngine.bids.length, 1)
+    assert.strictEqual(matchingEngine.bids[0].quantity, 3)
+  })
+
+  test(`
+      given
+        there are two ask orders with quantities 5 and 10 respectively
+      when
+        new bid order added with quantity 12 and
+        bid order price matches ask order prices
+      then
+        trade should happen for quantity 12 and
+        first ask order should fully execute and
+        second ask order should have remaining quantity 3
+    `, () => {
+    //assign
+    const matchingEngine = new MatchingEngine()
+    const askOrder1: Order = { id: randomUUID(), side: OrderSide.ASK, price: 1, quantity: 5 }
+    const askOrder2: Order = { id: randomUUID(), side: OrderSide.ASK, price: 1, quantity: 10 }
+    const bidOrder: Order = { id: randomUUID(), side: OrderSide.BID, price: 1, quantity: 12 }
+    matchingEngine.addOrder(askOrder1)
+    matchingEngine.addOrder(askOrder2)
+    // act
+    const result = matchingEngine.addOrder(bidOrder)
+    // assert
+    const trade1: Trade = {
+      bidOrderId: bidOrder.id,
+      askOrderId: askOrder1.id,
+      price: 1,
+      quantity: 5,
+    }
+    const trade2: Trade = {
+      bidOrderId: bidOrder.id,
+      askOrderId: askOrder2.id,
+      price: 1,
+      quantity: 7,
+    }
+    assert.deepEqual(result, [trade1, trade2])
+    assert.strictEqual(matchingEngine.asks.length, 1)
+    assert.strictEqual(matchingEngine.asks[0].quantity, 3)
   })
 })
