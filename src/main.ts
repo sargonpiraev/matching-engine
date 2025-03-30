1fbedbe89b50900bb636fc028796df585ec3adba
sargonpiraev <sargonpiraev@gmail.com>
2025-03-30 21:19:57 +0300

    add partial filling

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,9 @@ import { OrderSide } from './types'
 
 const matchingEngine = new MatchingEngine()
 
-matchingEngine.createOrder({ id: '1', price: 1, side: OrderSide.ASK })
-matchingEngine.createOrder({ id: '2', price: 1, side: OrderSide.BID })
+matchingEngine.addOrder({ id: '1', price: 1, side: OrderSide.ASK, quantity: 1 })
+matchingEngine.addOrder({ id: '2', price: 1, side: OrderSide.BID, quantity: 1 })
 
-matchingEngine.createOrder({ id: '3', price: 2, side: OrderSide.ASK })
-matchingEngine.createOrder({ id: '4', price: 1, side: OrderSide.BID })
-matchingEngine.createOrder({ id: '5', price: 3, side: OrderSide.BID })
+matchingEngine.addOrder({ id: '3', price: 2, side: OrderSide.ASK, quantity: 1 })
+matchingEngine.addOrder({ id: '4', price: 1, side: OrderSide.BID, quantity: 1 })
+matchingEngine.addOrder({ id: '5', price: 3, side: OrderSide.BID, quantity: 1 })
